9f2e57089e740acb5401d554cf7bea95c0091031
b1jam14 <b.zolver@gmail.com>
2025-08-27 13:30:36 +0200

    Test API

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 document.getElementById("button-nv").addEventListener("click", function() {
-    const testText = document.getElementById("test").value;
+    /*const testText = document.getElementById("test").value;
     console.log(testText);
     fetch('testapi.php?type=test', {
         method: 'POST',
@@ -8,7 +8,13 @@ document.getElementById("button-nv").addEventListener("click", function() {
           action: 'NVMATCH',
           text: testText
         })
-    }) .catch(error => console.error('Erreur API:', error));
+    }) .catch(error => console.error('Erreur API:', error));*/
+
+
+    const query = new Parse.Query("Games");
+    query.find().then((results) => {
+      results.forEach(obj => console.log(obj.id, obj.get("playerName")));
+    }).catch(error => console.error('Error:', error.message));  
 });
 document.getElementById("button-md").addEventListener("click", function() {
     const testText = document.getElementById("test").value;
