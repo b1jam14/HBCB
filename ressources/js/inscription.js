c81df91961663aa1776fec514431d332f673d92d
b1jam14 <48470211+b1jam14@users.noreply.github.com>
2025-09-10 21:16:03 +0200

    Update inscription.js

--- ressources/js/inscription.js
+++ ressources/js/inscription.js
@@ -78,7 +78,7 @@ document.getElementById('button-enter-save').addEventListener('click',async (e) 
 
   const acl = new Parse.ACL();
   acl.setPublicReadAccess(true); // everyone can read
-  acl.setWriteAccess(user.id, true); // only this user can write
+ // acl.setWriteAccess(user.id, true); // only this user can write
   user.setACL(acl);
 
   try {
